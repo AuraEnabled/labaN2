2f6fb4bb06e994dd7e1d12c3c4fb57452aa2f40f
Борис Бритва <boris.gribanov@gmail.com>
2019-11-18 12:51:49 +0200

    Добавлен автоматический бекап данных каждые 10 секунд в отдельном потоке

--- src/com/boris/Classes/BackUp.java
+++ src/com/boris/Classes/BackUp.java
@@ -1,8 +1,27 @@
 package com.boris.Classes;
 
+import com.boris.Main;
+
+import java.io.IOException;
+
 public class BackUp extends Thread{
+    private Serialization serialization = new Serialization("Patients.bcp");
+    private Patients patients;
+
+    public BackUp(Patients patients){
+        this.patients = patients;
+    }
     @Override
     public void run(){
+        while(true) {
+            try {
+                serialization.Serialization(patients);
+                Thread.sleep(10000);
+                System.out.println("BACKUP");
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
--- src/com/boris/Main.java
+++ src/com/boris/Main.java
@@ -1,5 +1,6 @@
 package com.boris;
 
+import com.boris.Classes.BackUp;
 import com.boris.Classes.Patient;
 import com.boris.Classes.Patients;
 
@@ -8,6 +9,7 @@ import java.io.*;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Arrays;
 //  list.forEach(System.out::println);
@@ -18,34 +20,38 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import static com.boris.Classes.Patient.Procedures.valueOf;
 import static java.lang.Thread.currentThread;
 //  import org.apache.commons.compress.archivers.ArchiveException;
 
 public class Main {
 
-    private String fullName;
-    private int id;
-    private String procedure;
-    private double inTotal;
-    private boolean paid;
-    private double debt;
+    public static void main(String[] args) throws IOException {
 
-        public static void main(String[] args) throws IOException {
+        String fullName;
+            int id;
+            Patient.Procedures procedure;
+            String procedureStr;
+            double inTotal;
+            boolean paid;
+            double debt;
+
         Patients patients = new Patients();
         Scanner scanner = new Scanner(System.in);
         Serialization serialization = new Serialization("Patients.ser");
-//        patients.getListOfPatients().add(new Patient("Жмышенко Валерий", 1, Patient.Procedures.Кастрация, 500, false, 500));
-//        patients.getListOfPatients().add(new Patient("Ананасов Александер", 228, Patient.Procedures.Чистка, 1000.0d, true, 0));
-//        patients.getListOfPatients().add(new Patient("Трипавловских Александр", 2, Patient.Procedures.Кастрация, 500, false, 500));
-//        patients.getListOfPatients().add(new Patient("Денис Орешкин", 7, Patient.Procedures.Пломбирование, 799.99, true, 0 ));
-//        patients.getListOfPatients().add(new Patient("Говнов Иван", 3, Patient.Procedures.Чистка, 500, true, 0));
-//        patients.getListOfPatients().add(new Patient("Летов Егор", 4, Patient.Procedures.Протезирование, 500, false, 500));
-//        patients.getListOfPatients().add(new Patient("Шмурдяк Антон", 14, Patient.Procedures.Кремация, 800, false, 400));
-//        patients.getListOfPatients().add(new Patient("Горин Геннадий", 5, Patient.Procedures.Протезирование, 500, false, 500));
-//        patients.getListOfPatients().add(new Patient("Белоглазов Анатолий", 88, Patient.Procedures.Чистка, 500, true, 0));
-//        patients.getListOfPatients().add(new Patient("Борис Бритва", 15, Patient.Procedures.Пломбирование, 350, true, 0));
-
-        System.out.println("Выберите действие\n\tПоиск по базе - 1\n\tСправки о должниках - 2\n\tУдалить записи без задолженностей - 3\n\tВывести данные по всем записям - 4\n\tИмя потока - 5\n\tСериализация - 6\n\tДесериализация - 7\n\t");
+        patients.getListOfPatients().add(new Patient("Жмышенко Валерий", 1, Patient.Procedures.Кастрация, 500, false, 500));
+        patients.getListOfPatients().add(new Patient("Ананасов Александер", 228, Patient.Procedures.Чистка, 1000.0d, true, 0));
+        patients.getListOfPatients().add(new Patient("Трипавловских Александр", 2, Patient.Procedures.Кастрация, 500, false, 500));
+        patients.getListOfPatients().add(new Patient("Денис Орешкин", 7, Patient.Procedures.Пломбирование, 799.99, true, 0 ));
+        patients.getListOfPatients().add(new Patient("Говнов Иван", 3, Patient.Procedures.Чистка, 500, true, 0));
+        patients.getListOfPatients().add(new Patient("Летов Егор", 4, Patient.Procedures.Протезирование, 500, false, 500));
+        patients.getListOfPatients().add(new Patient("Шмурдяк Антон", 14, Patient.Procedures.Кремация, 800, false, 400));
+        patients.getListOfPatients().add(new Patient("Горин Геннадий", 5, Patient.Procedures.Протезирование, 500, false, 500));
+        patients.getListOfPatients().add(new Patient("Белоглазов Анатолий", 88, Patient.Procedures.Чистка, 500, true, 0));
+        patients.getListOfPatients().add(new Patient("Борис Бритва", 15, Patient.Procedures.Пломбирование, 350, true, 0));
+        BackUp backUp = new BackUp(patients);
+        backUp.start();
+        System.out.println("Выберите действие\n\tПоиск по базе - 1\n\tСправки о должниках - 2\n\tУдалить записи без задолженностей - 3\n\tВывести данные по всем записям - 4\n\tИмя потока - 5\n\tСериализация - 6\n\tДесериализация - 7");
 
         Scanner choice = new Scanner(System.in);
         while (true) {
